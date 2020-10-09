57d57b85a382006f59c4e4346b63d9c992d4fe3f
Abrancillo <48803834+Abrancillo@users.noreply.github.com>
2020-10-08 20:29:12 -0500

    Primera subida

---
+++ DragonGolfBackEnd/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DragonGolfBackEnd.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DragonGolfBackEnd/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace DragonGolfBackEnd.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
