b17fdf8e733d99f8ae4b1e4edb8ee9f11a840ba7
Abrancillo <48803834+Abrancillo@users.noreply.github.com>
2020-10-26 14:54:13 -0600

    Jugadores 80%

---
+++ DragonGolfBackEnd/Controllers/Amigos/AltaAmigosController.cs
@@ -0,0 +1,112 @@
+﻿using DragonGolfBackEnd.Clases;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DragonGolfBackEnd.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*", SupportsCredentials = true)]
+    [RoutePrefix("api/AltaAmigos")]
+    public class AltaAmigosController : ApiController
+    {
+        public class ParametrosEntradas
+        {
+           
+            public int IDUsuarioFav { get; set; }
+            public int IDUsuario { get; set; }
+            public bool Fav_Status { get; set; }
+        }
+            public JObject Post(ParametrosEntradas Datos)
+            {
+                try
+                {
+                    SqlCommand comando = new SqlCommand("DragoGolf_AddFriend");
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    //Declaracion de parametros
+                    comando.Parameters.Add("@IDUsuarioFav", SqlDbType.Int);
+                    comando.Parameters.Add("@IDUsuario", SqlDbType.Int);
+                    comando.Parameters.Add("@Fav_Status", SqlDbType.Bit);
+                  
+
+                    //Asignacion de valores a parametros
+                    comando.Parameters["@IDUsuarioFav"].Value = Datos.IDUsuarioFav;
+                    comando.Parameters["@IDUsuario"].Value = Datos.IDUsuario;
+                    comando.Parameters["@Fav_Status"].Value = Datos.Fav_Status;
+                  
+
+                    comando.Connection = new SqlConnection(VariablesGlobales.CadenaConexion);
+                    comando.CommandTimeout = 0;
+                    comando.Connection.Open();
+
+                    DataTable DT = new DataTable();
+                    SqlDataAdapter DA = new SqlDataAdapter(comando);
+                    comando.Connection.Close();
+                    DA.Fill(DT);
+
+
+
+                    string Mensaje = "";
+                    int Estatus = 0;
+                    int IDCourse = 0;
+
+                int contador = DT.Rows.Count;
+
+                    if (DT.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in DT.Rows)
+                        {
+                            Mensaje = Convert.ToString(row["mensaje"]);
+                            Estatus = Convert.ToInt32(row["Estatus"]);
+                           
+                    }
+
+                        JObject Resultado = JObject.FromObject(new
+                        {
+                            mensaje = Mensaje,
+                            estatus = Estatus,
+                         
+                        });
+
+                        return Resultado;
+                    }
+                    else
+                    {
+                        JObject Resultado = JObject.FromObject(new
+                        {
+                            mensaje = Mensaje,
+                            estatus = Estatus,
+
+                        });
+
+                        return Resultado;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    JObject Resultado = JObject.FromObject(new
+                    {
+                        mensaje = ex.ToString(),
+                        estatus = 0,
+
+                    });
+
+                    return Resultado; //JsonConvert.SerializeObject(lista);
+                }
+            }
+
+
+        }
+    }
---
+++ DragonGolfBackEnd/Controllers/Amigos/ListaAmigosController.cs
@@ -0,0 +1,142 @@
+﻿using DragonGolfBackEnd.Clases;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DragonGolfBackEnd.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*", SupportsCredentials = true)]
+    [RoutePrefix("api/ListaAmigos")]
+    public class ListaAmigosController : ApiController
+    {
+
+        public class ParametrosEntradas
+        {
+            public int IDUsuario { get; set; }
+        }
+
+
+        public class ParametrosSalida
+        {
+            public int IDUsuario { get; set; }
+            public string usu_nombre { get; set; }
+            public string usu_apellido_paterno { get; set; }
+            public string usu_apellido_materno { get; set; }
+            public string usu_email { get; set; }
+            public string usu_nickname { get; set; }
+            public string usu_telefono { get; set; }
+            public string usu_imagen { get; set; }
+
+            public decimal usu_handicapindex { get; set; }
+            public int usu_ghinnumber { get; set; }
+        }
+
+        public JObject Post(ParametrosEntradas Datos)
+        {
+            try
+            {
+                SqlCommand comando = new SqlCommand("DragoGolf_ListCourse");
+                comando.CommandType = CommandType.StoredProcedure;
+
+                //Declaracion de parametros
+                comando.Parameters.Add("@IDUsuario", SqlDbType.VarChar);
+
+
+                //Asignacion de valores a parametros
+                comando.Parameters["@IDUsuario"].Value = Datos.IDUsuario;
+
+
+                comando.Connection = new SqlConnection(VariablesGlobales.CadenaConexion);
+                comando.CommandTimeout = 0;
+                comando.Connection.Open();
+
+                DataTable DT = new DataTable();
+                SqlDataAdapter DA = new SqlDataAdapter(comando);
+                comando.Connection.Close();
+                DA.Fill(DT);
+
+                List<ParametrosSalida> lista = new List<ParametrosSalida>();
+
+                string Mensaje = "";
+                int Estatus = 0;
+
+                int contador = DT.Rows.Count;
+
+                if (DT.Rows.Count > 0)
+                {
+                    foreach (DataRow row in DT.Rows)
+                    {
+                        Mensaje = Convert.ToString(row["mensaje"]);
+                        Estatus = Convert.ToInt32(row["Estatus"]);
+
+                        if (Estatus == 1)
+                        {
+                            ParametrosSalida ent = new ParametrosSalida
+                            {
+                             IDUsuario = Convert.ToInt32(row["IDCourse"]),
+       usu_nombre = Convert.ToString(row["IDCourse"]),
+        usu_apellido_paterno = Convert.ToString(row["IDCourse"]),
+         usu_apellido_materno = Convert.ToString(row["IDCourse"]),
+         usu_email = Convert.ToString(row["IDCourse"]),
+         usu_nickname = Convert.ToString(row["IDCourse"]),
+         usu_telefono = Convert.ToString(row["IDCourse"]),
+         usu_imagen = Convert.ToString(row["IDCourse"]),
+         usu_handicapindex = Convert.ToDecimal(row["IDCourse"]),
+        usu_ghinnumber = Convert.ToInt32(row["IDCourse"]),
+
+    };
+
+                            lista.Add(ent);
+                        }
+                    }
+
+                    JObject Resultado = JObject.FromObject(new
+                        {
+                            mensaje = Mensaje,
+                            estatus = Estatus,
+                             Result = lista
+
+                    });
+
+                        return Resultado;
+                    }
+                else
+                    {
+                        JObject Resultado = JObject.FromObject(new
+                        {
+                            mensaje = Mensaje,
+                            estatus = Estatus,
+
+                        });
+
+                        return Resultado;
+                    }
+
+                }
+            catch (Exception ex)
+            {
+
+                JObject Resultado = JObject.FromObject(new
+                {
+                    mensaje = ex.ToString(),
+                    estatus = 0,
+
+                });
+
+                return Resultado; //JsonConvert.SerializeObject(lista);
+            }
+        }
+
+
+    }
+}
