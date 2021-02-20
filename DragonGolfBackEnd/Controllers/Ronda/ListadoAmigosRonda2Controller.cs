c7a792a1d172d50a9c2f301b787a789702fa5d72
Abrancillo <48803834+Abrancillo@users.noreply.github.com>
2021-02-20 00:57:24 -0600

    adecuacion menor

--- DragonGolfBackEnd/Controllers/Ronda/ListadoAmigosRonda2Controller.cs
+++ DragonGolfBackEnd/Controllers/Ronda/ListadoAmigosRonda2Controller.cs
@@ -53,7 +53,7 @@ namespace DragonGolfBackEnd.Controllers
         {
             try
             {
-                SqlCommand comando = new SqlCommand("DragoGolf_ListFriendRound");
+                SqlCommand comando = new SqlCommand("DragoGolf_ListFriendRound2");
                 comando.CommandType = CommandType.StoredProcedure;
 
                 //Declaracion de parametros
